a6a6b7c98fc9db1e3c4453d2b01b6e0f072fbe75
Diego Dorado <diegodorado@gmail.com>
2015-12-27 01:25:37 -0300

    adds ofxDomemaster addon and working example

---
+++ Graphics/examples/ofxDomemasterExample/src/main.cpp
@@ -0,0 +1,12 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+
+	ofSetupOpenGL(1024,1024,OF_WINDOW);			// <-------- setup the GL context
+
+
+	ofRunApp(new ofApp());
+
+}
