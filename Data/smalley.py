f6bb285e7b7f4452537b9f347c63516220882525
Diego Dorado <diegodorado@gmail.com>
2016-03-28 01:07:57 -0300

    Adds new terms to smalley vocabulary implementation

--- Data/smalley.py
+++ Data/smalley.py
@@ -10,27 +10,64 @@ interval = 1.12 # time between events, in seconds
 def printc(t, c='o'):
     print '\033[9' + {'o': '2m','w': '3m','e': '1m'}[c] + t + '\033[0m'
 
-onset_types = ['','departure','emergence','anacrusis','attack','upbeat','downbeat']
-continuant_types = ['','passage','transition','prolongation','maintenance','statement']
-termination_types = ['','arrival','disappearence','closure','release','resolution','plane']
-spectrum_types = ['harmonic', 'inharmonic','granular','saturated']
+onsets = ['','departure','emergence','anacrusis','attack','upbeat','downbeat']
+continuants = ['','passage','transition','prolongation','maintenance','statement']
+terminations = ['','arrival','disappearence','closure','release','resolution','plane']
+morfological_functions = [onsets,continuants,terminations]
+duration_ranges = [ [0.01, 0.3], [0.4, 2], [0.2, 0.5]]
+spectrums = ['harmonic', 'inharmonic','granular','saturated']
+spectral_occupation_types = ['canopy', 'centre', 'root']
+spectral_densities = ['filled', 'packed', 'opaque','translucent','transparent','empty']
+
+unidirectional_motions = ['','ascent','plane','descent']
+reciprocal_motions = ['','parabola','oscilation','ondulation']
+cyclic_motions = ['','rotation','spiral','spin','vortex','pericentrality','centrifugal']
+characteristic_motions = ['','push','flow','rise','throw','drift','float','fly','plummet','dive','fall','tumble','sink','glide','slump']
+
+texture_motion_relationships = ['','streaming', 'flocking', 'convolution','turbulence']
+
+
+
 
 def send_event():
     try:
         bundle = OSCBundle()
-        msg = OSCMessage("/entry")
-        #onset
-        msg.append(choice(onset_types))
-        msg.append(uniform(0.01, 0.3)) #duration
-        msg.append(choice(spectrum_types))
-        #continuant
-        msg.append(choice(continuant_types))
-        msg.append(uniform(0.4, 2)) #duration
-        msg.append(choice(spectrum_types))
-        #termination
-        msg.append(choice(termination_types))
-        msg.append(uniform(0.2, 0.5)) #duration
-        msg.append(choice(spectrum_types))
+        msg = OSCMessage("/sound_unit")
+
+        for i in range(3):
+            #onset, continuant, termination
+            msg.append(choice(morfological_functions[i]))
+            #duration
+            msg.append(uniform(duration_ranges[i][0],duration_ranges[i][1]))
+            # unidirectional_motions
+            msg.append(choice(unidirectional_motions))
+            # reciprocal_motions
+            msg.append(choice(reciprocal_motions))
+            # cyclic_motions
+            msg.append(choice(cyclic_motions))
+            # characteristic_motions
+            msg.append(choice(characteristic_motions))
+
+            # texture motion
+            msg.append(choice(texture_motion_relationships))
+            msg.append(uniform(0, 1)) # continuity-discontinuity (sustained -> granular -> iterative)
+            msg.append(uniform(0, 1)) # movement:  periodic - aperiodic erratic
+            msg.append(uniform(0, 1)) # movement:  accelerating - decelerating - flux
+
+            # spectrum type
+            msg.append(choice(spectrums))
+
+            #spectral occupation type
+            msg.append(choice(spectral_occupation_types))
+            #spectral qualifiers
+            msg.append(uniform(0, 1)) # emptiness - plenitude
+            msg.append(uniform(0, 1)) # diffuseness - concentration
+            msg.append(uniform(0, 1)) # streams - interstices
+            msg.append(uniform(0, 1)) # overlap - crossover
+
+            #spectral density
+            msg.append(choice(spectral_densities))
+
 
         bundle.append(msg)
         client.send(bundle)
@@ -70,3 +107,45 @@ except OSCClientError:
 
 except KeyboardInterrupt:
     print "\nProgram terminated."
+
+
+'''
+
+
+Sound Unit
+
+	Morfological framework
+		Function: Onsets, Continuants and Terminations
+		Description (palabras para cada categoria)
+		Duration (float, en segundos)
+		Motion and Growth
+			Motion: unidirectional, reciprocal and cyclic/centric;
+			Growth: bi/multidirectional
+			Characteristics of motion
+		Texture motion :
+            relationship: streaming, flocking, convolution and turbulence.
+			internal consistency: continuity-discontinuity continuum
+								(sustained -> granular -> iterative)
+			movement: 	o periodic - aperiodic erratic
+						o accelerating - decelerating - flux
+						o grouping patterns
+		Behaviour
+			motion coordination (vertical): loose-tight continuum
+			motion passage (Causality, horiz): voluntary-pressured cont.
+			relations: dominance/subordination and conflict/coexistence.
+
+	Spectral framework
+        Note to Noise continuum (armonico, inarmonico, granular, saturado)
+                                (desarrollar esto)
+        Occupancy of Spectral Space
+	       type:  canopy, centre and root
+           qualifiers:
+		        o emptiness - plenitude
+		        o diffuseness - concentration
+		        o streams - interstices
+		        o overlap - crossover
+		Spectral Density (filled, packed/compressed, opaque, translucent, transparent and empty)
+
+
+
+'''
