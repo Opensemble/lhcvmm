544883c16f9f7a861252a6d557f54383e9e7f980
Diego Dorado <diegodorado@gmail.com>
2016-03-30 00:29:55 -0300

    finish example loading csv file to drive osc messages

--- Data/smalley.py
+++ Data/smalley.py
@@ -4,18 +4,29 @@ from thread import start_new_thread
 from OSC import  OSCClient, OSCClientError, OSCBundle, OSCMessage
 from random import random, choice, uniform
 import argparse
+import csv
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-s','--simulate', help='Simulate events.', action='store_true')
+parser.add_argument('-c','--csv', help='Load events from csv.', action='store_false')
 parser.add_argument('-r','--random', help='Send random events. (will use a list otherwise)', action='store_true')
 
 args = parser.parse_args()
 
-
+csv_data = []
+# columns that must be converted to float
+float_indexes = [0,2,8,9,10,13,14,15,16,19,25,26,27,30,31,32,33,36,42,43,44,47,48,49,50]
+with open('data_files/smalley.csv', 'rb') as csvfile:
+    csv = csv.reader(csvfile, delimiter=',', quotechar='"')
+    csv.next() # first row
+    csv.next() # and second row are headers
+    for row in csv:
+        for i, val in enumerate(row):
+            if i in float_indexes:
+                row[i] = float(row[i].replace(",", "."))
+        csv_data.append(row)
 
 
 osc_port = 57120 #default SuperCollider port (must be open before executing this program)
-interval = 1.12 # time between events, in seconds
 # just some easy ansi colors printing: 'o' for ok, 'w' for warning, 'e' for error.
 def printc(t, c='o'):
     print '\033[9' + {'o': '2m','w': '3m','e': '1m'}[c] + t + '\033[0m'
@@ -37,76 +48,16 @@ spectrums = ['harmonic', 'inharmonic','granular','saturated']
 spectral_occupation_types = ['canopy', 'centre', 'root']
 spectral_densities = ['filled', 'packed', 'opaque','translucent','transparent','empty']
 
+# https://docs.google.com/feeds/download/spreadsheets/Export?key=16FjhAmzO4CZyGDEJfdMguKVnG9FgMQe8M04X5MLigR4&exportFormat=csv
 
 
 
-# test osc messages:
-
-# onset, continuant, termination
-# duration
-# unidirectional_motions
-# reciprocal_motions
-# cyclic_motions
-# characteristic_motions
-# texture motion
-# continuity-discontinuity (sustained -> granular -> iterative)
-# movement:  periodic - aperiodic erratic
-# movement:  accelerating - decelerating - flux
-# spectrum type
-# spectral occupation type
-# emptiness - plenitude
-# diffuseness - concentration
-# streams - interstices
-# overlap - crossover
-# spectral density
-'departure', 0.2, 'ascent','parabola','rotation','push', 'flocking', 0.5, 0.5, 0.5, 'harmonic', 'canopy', 0.5, 0.5, 0.5, 0.5, 'filled'
-
-
-
-msg.append(choice(spectral_densities))
-
-
-
-
-def send_random_event():
+def send_event(data):
     try:
         bundle = OSCBundle()
         msg = OSCMessage("/sound_unit")
-
-        for i in range(3):
-            #onset, continuant, termination
-            msg.append(choice(morfological_functions[i]))
-            #duration
-            msg.append(uniform(duration_ranges[i][0],duration_ranges[i][1]))
-            # unidirectional_motions
-            msg.append(choice(unidirectional_motions))
-            # reciprocal_motions
-            msg.append(choice(reciprocal_motions))
-            # cyclic_motions
-            msg.append(choice(cyclic_motions))
-            # characteristic_motions
-            msg.append(choice(characteristic_motions))
-
-            # texture motion
-            msg.append(choice(texture_motion_relationships))
-            msg.append(uniform(0, 1)) # continuity-discontinuity (sustained -> granular -> iterative)
-            msg.append(uniform(0, 1)) # movement:  periodic - aperiodic erratic
-            msg.append(uniform(0, 1)) # movement:  accelerating - decelerating - flux
-
-            # spectrum type
-            msg.append(choice(spectrums))
-
-            #spectral occupation type
-            msg.append(choice(spectral_occupation_types))
-            #spectral qualifiers
-            msg.append(uniform(0, 1)) # emptiness - plenitude
-            msg.append(uniform(0, 1)) # diffuseness - concentration
-            msg.append(uniform(0, 1)) # streams - interstices
-            msg.append(uniform(0, 1)) # overlap - crossover
-
-            #spectral density
-            msg.append(choice(spectral_densities))
-
+        for d in data:
+            msg.append(d)
 
         bundle.append(msg)
         client.send(bundle)
@@ -114,29 +65,86 @@ def send_random_event():
     except OSCClientError, e:
         printc( "\OSCClientError: Connection refused on port %s." % osc_port, 'e')
 
+
+def send_random_event():
+    data = []
+
+    for i in range(3):
+        #onset, continuant, termination
+        data.append(choice(morfological_functions[i]))
+        #duration
+        data.append(uniform(duration_ranges[i][0],duration_ranges[i][1]))
+        # unidirectional_motions
+        data.append(choice(unidirectional_motions))
+        # reciprocal_motions
+        data.append(choice(reciprocal_motions))
+        # cyclic_motions
+        data.append(choice(cyclic_motions))
+        # characteristic_motions
+        data.append(choice(characteristic_motions))
+
+        # texture motion
+        data.append(choice(texture_motion_relationships))
+        data.append(uniform(0, 1)) # continuity-discontinuity (sustained -> granular -> iterative)
+        data.append(uniform(0, 1)) # movement:  periodic - aperiodic erratic
+        data.append(uniform(0, 1)) # movement:  accelerating - decelerating - flux
+
+        # spectrum type
+        data.append(choice(spectrums))
+
+        #spectral occupation type
+        data.append(choice(spectral_occupation_types))
+        #spectral qualifiers
+        data.append(uniform(0, 1)) # emptiness - plenitude
+        data.append(uniform(0, 1)) # diffuseness - concentration
+        data.append(uniform(0, 1)) # streams - interstices
+        data.append(uniform(0, 1)) # overlap - crossover
+
+        #spectral density
+        data.append(choice(spectral_densities))
+
+    send_event(data)
+
 # Define a function for the thread
 def send_events_periodically():
-    print "Start Sending events every %s seconds." % interval
-    print "Each * printed represents an event sent."
-    print "Press Ctrl + C to finalize."
-
     starttime=time.time()
 
-    while 1:
-        # restart * printing every 10 events
-        sys.stdout.write('\r----------\r')
-        for i in range(0, 10):
-            if args.simulate:
-                if args.random:
-                    send_random_event()
-                    time_to_sleep = interval - ((time.time() - starttime) % interval)
-                else:
-                    send_random_event()
-                    time_to_sleep = interval - ((time.time() - starttime) % interval)
+    if args.random:
+        interval = 1.12 # time between events, in seconds
+        print "Start Sending events every %s seconds." % interval
+        print "Each * printed represents an event sent."
+        print "Press Ctrl + C to finalize."
+        while 1:
+            # restart * printing every 10 events
+            sys.stdout.write('\r----------\r')
+            for i in range(0, 10):
+                send_random_event()
+                time_to_sleep = interval - ((time.time() - starttime) % interval)
+                sys.stdout.write('*')
+                sys.stdout.flush()
+                time.sleep(time_to_sleep)
 
-            sys.stdout.write('*')
-            sys.stdout.flush()
-            time.sleep(time_to_sleep)
+    if args.csv:
+        print "Start Sending events from csv file."
+        print "Each * printed represents an event sent."
+        print "Press Ctrl + C to finalize."
+        while 1:
+
+            csv_data_index = 0
+
+            sys.stdout.write('\r')
+            for i in range(len(csv_data)):
+                sys.stdout.write('-')
+            sys.stdout.write('\r')
+
+            for row in csv_data:
+                interval = row[0]
+                time_to_sleep = interval - ((time.time() - starttime) % interval)
+                send_event(row[1:])
+                sys.stdout.write('*')
+                sys.stdout.flush()
+                time.sleep(time_to_sleep)
+
 
 
 try:
